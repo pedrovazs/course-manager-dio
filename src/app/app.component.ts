104c3880667e781c8b217167a79a92e923e42e8c
Pedro Vaz <pedrovazs@outlook.com>
2021-08-30 18:29:16 -0300

    Explicação, módulos e data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Decorator para dar mais funções
 @Component({
+  // Cria uma diretiva que no final será transformado numa tag html
   selector: 'app-root',
+  // Aqui diz para o Angular qual html que será usado para este componente
   templateUrl: './app.component.html',
+  // Aqui diz para o Angular que o style que será usado estará sendo passado por aqui
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'course-manager';
+
+  // Em TypeScript podemos tipar as nossas variáveis, mostrando que tipo de valor se
+  // espera em uma variável
+  name: string = 'John';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // Aqui declaramos todos os nossos componentes
   declarations: [
     AppComponent
   ],
+  // Usado para quando for importar um módulo
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
