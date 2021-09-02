4209c05b7f62e4b94bd43a2a42c46476dbc3fb0e
Pedro Vaz <pedrovazs@outlook.com>
2021-09-02 10:55:39 -0300

    Aplicando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Erro404Component } from './Erro404/Erro404.component';
 
 @NgModule({
   // Aqui declaramos todos os nossos componentes
@@ -14,12 +17,29 @@ import { ReplacePipe } from './pipe/replace.pipe';
     AppComponent,
     CourseListComponent,
     StarComponent,
+    NavBarComponent,
+    Erro404Component,
     ReplacePipe
   ],
   // Usado para quando for importar um módulo
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,7 +8,6 @@ import { CourseService } from "./course.service";
 
 // Este decorator recebe um objeto
 @Component({
-    selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
